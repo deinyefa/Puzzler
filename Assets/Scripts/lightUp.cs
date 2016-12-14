285d1f6303e04b0cc8ee4377b99e90995782c255
Deinyefa Eporwei <df.eporwei@gmail.com>
2016-12-14 12:27:33 -0500

    feat: core game mechanics and audio/visual aesthetics

--- Assets/Scripts/lightUp.cs
+++ Assets/Scripts/lightUp.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 
 public class lightUp : MonoBehaviour {
-	/*public Material lightUpMaterial;
+	public Material lightUpMaterial;
 	public GameObject gameLogic;
 	private Material defaultMaterial;
 
 	// Use this for initialization
 	void Start () {
-		defaultMaterial = this.GetComponent<MeshRenderer> ().material; //Save our initial material as the default
-		this.GetComponentInChildren<ParticleSystem>().enableEmission = false; //Start without emitting particles
-
-		gameLogic = GameObject.Find ("gameLogic");
+		defaultMaterial = GetComponent<MeshRenderer>().material; //Save our initial material as the default
+        GetComponentInChildren<ParticleSystem>().enableEmission=false; //Start without emitting particles
+        
+        gameLogic = GameObject.Find ("gameLogic");
 	}
 	
 	// Update is called once per frame
@@ -24,27 +24,27 @@ public class lightUp : MonoBehaviour {
 
 
 	public void gazeLightUp() {
-		this.GetComponent<MeshRenderer>().material = lightUpMaterial; //Assign the hover material
-		this.GetComponentInChildren<ParticleSystem>().enableEmission = true; //Turn on particle emmission
-		this.GetComponent<GvrAudioSource>().Play();
+        GetComponent<MeshRenderer>().material = lightUpMaterial; //Assign the hover material
+        GetComponentInChildren<ParticleSystem>().enableEmission = true; //Turn on particle emmission
+        GetComponent<GvrAudioSource>().Play();
 
-		//gameLogic.GetComponent<gameLogic>().playerSelection(this.gameObject);
+		gameLogic.GetComponent<GameLogic>().playerSelection(this.gameObject);
 
 
 	}
 	public void playerSelection() {
-		//GameLogic.GetComponent<gameLogic>().playerSelection(this.gameObject);
-		//this.GetComponent<GvrAudioSource>().Play();
+		gameLogic.GetComponent<GameLogic>().playerSelection(this.gameObject);
+		this.GetComponent<GvrAudioSource>().Play();
 	}
 	public void aestheticReset() {
-		this.GetComponent<MeshRenderer>().material = defaultMaterial; //Revert to the default material
-		this.GetComponentInChildren<ParticleSystem>().enableEmission = false; //Turn off particle emission
+        GetComponent<MeshRenderer>().material = defaultMaterial; //Revert to the default material
+        GetComponentInChildren<ParticleSystem>().enableEmission = false; //Turn off particle emission
 	}
 
 	public void patternLightUp() { //Lightup behavior when the pattern shows.
-		this.GetComponent<MeshRenderer>().material = lightUpMaterial; //Assign the hover material
-		this.GetComponentInChildren<ParticleSystem>().enableEmission = true; //Turn on particle emmission
-		this.GetComponent<GvrAudioSource> ().Play (); //Play the audio attached
+        GetComponent<MeshRenderer>().material = lightUpMaterial; //Assign the hover material
+        GetComponentInChildren<ParticleSystem>().enableEmission = true; //Turn on particle emmission
+        GetComponent<GvrAudioSource>().Play (); //Play the audio attached
 	}
 
 
@@ -52,5 +52,5 @@ public class lightUp : MonoBehaviour {
 		patternLightUp ();
 		yield return new WaitForSeconds(duration-.1f);
 		aestheticReset ();
-	}*/
+	}
 }
